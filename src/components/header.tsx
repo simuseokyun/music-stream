a6879bfd6a8ed4c7ff02dbf82df5de44ded48dcd
simuseokyun <sim31059999@gmail.com>
2024-03-30 00:24:41 +0900

    style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,36 @@
 import { FormEvent } from 'react';
 import { useForm } from 'react-hook-form';
+import { useRecoilState } from 'recoil';
+import styled from 'styled-components';
+import { searchState } from '../atoms';
 
 interface ITrack {
     track: string;
 }
+const Container = styled.div`
+    padding: 20px;
+`;
+const Form = styled.form`
+    text-align: center;
+`;
+const Input = styled.input`
+    display: inline-block;
+    width: 300px;
+    outline: none;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid transparent;
+    background-color: rgb(40, 40, 40);
+    &:hover {
+        background-color: rgb(40, 40, 40);
+    }
+    &:focus {
+        border: 1px solid white;
+    }
+`;
 
 export const Header = () => {
+    const [search, setSearch] = useRecoilState(searchState);
     const {
         register,
         setValue,
@@ -13,22 +38,24 @@ export const Header = () => {
         formState: { errors },
     } = useForm<ITrack>();
     const onValid = ({ track }: ITrack) => {
+        setSearch(track);
         setValue('track', '');
     };
+    console.log(search);
     return (
-        <div>
-            <form onSubmit={handleSubmit(onValid)}>
-                <input
+        <Container>
+            <Form onSubmit={handleSubmit(onValid)}>
+                <Input
                     type="text"
                     placeholder="Search for Music"
                     {...register('track', {
                         minLength: { value: 1, message: '한 글자 이상 입력하세요' },
-                        required: true,
+                        required: { value: true, message: '필수 값 입니다' },
                     })}
                 />
-                <button type="submit">Search</button>
+                {/* <button type="submit">Search</button> */}
                 <p>{errors.track?.message || null}</p>
-            </form>
-        </div>
+            </Form>
+        </Container>
     );
 };
