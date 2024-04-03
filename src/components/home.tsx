1dc75c1856472e6d0d122cd532a5b4e02612892f
simuseokyun <sim31059999@gmail.com>
2024-04-03 16:24:54 +0900

    feat

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,12 +1,12 @@
 import { useQuery } from 'react-query';
-import { getArtist, getNewAlbum, getToken, searchAlbum, searchTrack } from '../api';
+import { getAlbum, getArtist, getNewAlbum, getToken, searchAlbum, searchTrack } from '../api';
 import { useState } from 'react';
 import styled from 'styled-components';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { Link } from 'react-router-dom';
 import { searchState, tokenValue } from '../atoms';
 import { Outlet } from 'react-router-dom';
-
+import { Button } from './button';
 import { NewAlbum } from './newAlbum';
 
 interface TrackImgProps {
@@ -14,7 +14,40 @@ interface TrackImgProps {
 }
 const Container = styled.div`
     width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    gap: 20px;
 `;
+const LeftMenuWrap = styled.div`
+    width: 100%;
+    height: 500px;
+    position: sticky;
+    top: 70px;
+`;
+const LeftMenuTop = styled.ul`
+    height: 100px;
+    background-color: #131212;
+    border-radius: 8px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 20px;
+`;
+const MenuTopList = styled.li`
+    font-size: 18px;
+    &:first-child {
+        margin-bottom: 20px;
+    }
+`;
+const LeftMenuBot = styled.div`
+    height: 400px;
+    margin-top: 20px;
+    background-color: #131212;
+    border-radius: 8px;
+    padding: 20px;
+`;
+
 const TrackImg = styled.div<{ url: string }>`
     background-image: url(${(props) => props.url});
     width: 50px;
@@ -109,30 +142,51 @@ const AlbumTitle = styled.td`
 export const Home = () => {
     const search = useRecoilValue(searchState);
     const setToken = useSetRecoilState(tokenValue);
-    const { isLoading: tokenLoading, data: tokenData } = useQuery<TokenResponse>('getToken', getToken);
-
-    const { isLoading: TrackLoading, data: trackData } = useQuery<ITracks>(['searchTrack', search], async () => {
-        if (!tokenLoading) {
-            const trackData = await searchTrack(tokenData?.access_token!, search);
-
-            return trackData;
-        }
-    });
-    const { isLoading: newAlbumLoading, data: newAlbumData } = useQuery<INewAlbum>('newAlbum', async () => {
-        if (!tokenLoading) {
-            return getNewAlbum(tokenData?.access_token!);
-        }
-    });
-    console.log(newAlbumLoading, newAlbumData, tokenData);
-
     const token = useRecoilValue(tokenValue);
-    console.log(token);
+    const { isLoading: tokenLoading, data: tokenData } = useQuery<TokenResponse>('getToken', getToken, {
+        onSuccess: (data) => {
+            setToken(data?.access_token!);
+        },
+    });
+
+    // const { isLoading: TrackLoading, data: trackData } = useQuery<ITracks>(['searchTrack', search], async () => {
+    //     if (!tokenLoading) {
+    //         const trackData = await searchTrack(tokenData?.access_token!, search);
+
+    //         return trackData;
+    //     }
+    // });
+
+    const { isLoading: newAlbumLoading, data: newAlbumData } = useQuery<INewAlbum>(
+        'newAlbum',
+        async () => {
+            if (!tokenLoading && tokenData?.access_token) {
+                return getNewAlbum(tokenData.access_token);
+            }
+        },
+        {
+            enabled: !tokenLoading && !!tokenData?.access_token, //
+        }
+    );
 
     return (
         <Container>
-            <Outlet />
+            <LeftMenuWrap>
+                <LeftMenuTop>
+                    <MenuTopList>
+                        <Link to="/">홈</Link>
+                    </MenuTopList>
+                    <MenuTopList>검색하기</MenuTopList>
+                </LeftMenuTop>
+                <LeftMenuBot>
+                    <h1>나의 라이브러리</h1>
+                    <Button text="플레이리스트" />
+                    <Button text="내가 찜한 앨범" />
+                </LeftMenuBot>
+            </LeftMenuWrap>
+            {/* <Outlet /> */}
             {newAlbumData?.albums && <NewAlbum newAlbums={newAlbumData?.albums!} />}
-            <div style={{ padding: '20px' }}>
+            {/* <div style={{ padding: '20px' }}>
                 <table style={{ width: '100%', verticalAlign: 'middle' }}>
                     <tr>
                         <th>앨범 커버</th>
@@ -156,7 +210,7 @@ export const Home = () => {
                               );
                           })}
                 </table>
-            </div>
+            </div> */}
         </Container>
     );
 };
