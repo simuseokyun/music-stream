aed0483530b6da8f526b1afa6d76bad95f0f9b05
simuseokyun <sim31059999@gmail.com>
2024-04-03 23:04:09 +0900

    feat : openPlaylist 상태 추가

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,5 +1,11 @@
 import { atom } from 'recoil';
 
+export enum typeTransform {
+    single = '싱글',
+    album = '앨범',
+    ep = 'EP',
+}
+
 export const searchState = atom({
     key: 'searchState',
     default: '',
@@ -8,4 +14,9 @@ export const searchState = atom({
 export const tokenValue = atom({
     key: 'tokenValue',
     default: '',
+});
+
+export const addPlaylistState = atom({
+    key: 'addPlaylistState',
+    default: false,
 });
