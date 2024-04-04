d02b1531a050672fc99e563c3465b34a41e8ac8b
simuseokyun <sim31059999@gmail.com>
2024-04-04 17:46:23 +0900

    style

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,9 +7,10 @@ const Container = styled.button`
     /* display: inline-block; */
     background-color: #ffffff;
     color: black;
-    padding: 8px;
+    padding: 4px 8px;
     border-radius: 20px;
     border: none;
+    font-size: 10px;
 `;
 
 export const Button = ({ text }: IButton) => {
