aeac2c548aaa83ef732a5ddf0563cbe920342956
simuseokyun <sim31059999@gmail.com>
2024-04-11 20:24:34 +0900

    feat : 분기처리

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,8 @@
 import { FormEvent } from 'react';
 import { useForm } from 'react-hook-form';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import styled from 'styled-components';
-import { searchState } from '../atoms';
+import { openSearch, searchState } from '../atoms';
 import { useNavigate } from 'react-router-dom';
 
 interface ITrack {
@@ -14,11 +14,13 @@ const Container = styled.div`
 const Form = styled.form`
     text-align: center;
 `;
-const Input = styled.input`
+const Input = styled.input<{ open: boolean }>`
     display: inline-block;
+    transform: translateY(${(props) => (props.open ? 0 : '-100px')});
+    transition: all 0.5s;
     width: 300px;
-    outline: none;
     padding: 10px;
+    outline: none;
     border-radius: 20px;
     color: white;
     border: 1px solid transparent;
@@ -34,6 +36,7 @@ const Input = styled.input`
 export const Header = () => {
     const [search, setSearch] = useRecoilState(searchState);
     const navigate = useNavigate();
+    const openSearch_ = useRecoilValue(openSearch);
     const {
         register,
         setValue,
@@ -45,11 +48,12 @@ export const Header = () => {
         setValue('track', '');
         navigate('search');
     };
-    console.log(search);
+
     return (
         <Container>
             <Form onSubmit={handleSubmit(onValid)}>
                 <Input
+                    open={openSearch_}
                     type="text"
                     placeholder="Search for Music"
                     {...register('track', {
@@ -57,8 +61,6 @@ export const Header = () => {
                         required: { value: true, message: '필수 값 입니다' },
                     })}
                 />
-                {/* <button type="submit">Search</button> */}
-                <p>{errors.track?.message || null}</p>
             </Form>
         </Container>
     );
