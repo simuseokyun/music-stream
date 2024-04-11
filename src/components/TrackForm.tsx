6f407c9f241ecce1109b271436a4a5dc62162bfd
simuseokyun <sim31059999@gmail.com>
2024-04-11 20:21:56 +0900

    주석 처리

--- src/components/TrackForm.tsx
+++ src/components/TrackForm.tsx
@@ -1,16 +1,1 @@
 import styled from 'styled-components';
-
-// const Container = styled.ul`
-//     background-color: black;
-//     color: white;
-// `;
-// const TrackList = styled.li`
-//     display: flex;
-// `;
-// const TrackImg = styled.div<{ image: string }>`
-//     background-image: url(${props}=>props.image);
-// `;
-
-// export const TrackForm = () => {
-//     return <Container></Container>;
-// };
