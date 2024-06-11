99d076c441aca35d7aca36b58ba654459b9dace6
simuseokyun <sim31059999@gmail.com>
2024-06-11 17:29:03 +0900

    refactor : 주석제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import dotenv from 'dotenv';
-import App from './root';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { CookiesProvider } from 'react-cookie';
-// dotenv.config();
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
 const client = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -16,7 +15,9 @@ root.render(
         <RecoilRoot>
             <QueryClientProvider client={client}>
                 <CookiesProvider>
-                    <RouterProvider router={router}></RouterProvider>
+                    <ThemeProvider theme={theme}>
+                        <RouterProvider router={router}></RouterProvider>
+                    </ThemeProvider>
                 </CookiesProvider>
             </QueryClientProvider>
         </RecoilRoot>
