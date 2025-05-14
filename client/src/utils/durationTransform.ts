b34d5eeb81b71c92ae218fa0dd643ac1f2693923
simuseokyun <sim31059999@gmail.com>
2025-05-14 17:05:47 +0900

    delete : 필요없는 파일 삭제

--- client/src/utils/durationTransform.ts
+++
@@ -1,6 +0,0 @@
-export const durationTransform = (duration: number) => {
-    const totalSeconds = duration / 1000;
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = Math.floor(totalSeconds % 60);
-    return { minutes, seconds };
-};
