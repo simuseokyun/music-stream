4df04271821436575323453633b0a55451dac54c
simuseokyun <sim31059999@gmail.com>
2025-06-10 23:00:52 +0900

    Delete : 파일 변경

--- client/src/utils/commaSeparate.ts
+++
@@ -1,4 +0,0 @@
-const commaSeparate = (num: number = 0) => {
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-};
-export default commaSeparate;
--- client/src/utils/extractAuthCodeFromUrl.ts
+++
@@ -1,5 +0,0 @@
-const extractAuthCodeFromUrl = (url: string) => {
-    const params = new URLSearchParams(url.split('?')[1]);
-    return params.get('code');
-};
-export default extractAuthCodeFromUrl;
