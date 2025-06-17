c493ea2c1b923ff2cb78e5eb60e456b59c67228a
simuseokyun <sim31059999@gmail.com>
2025-06-17 21:17:55 +0900

    Refactor : 플레이리스트/앨범 호출 API 커스텀 훅 분리

---
+++ client/src/hooks/album/useGetMyAlbums.tsx
@@ -0,0 +1,44 @@
+import { useInView } from 'react-intersection-observer';
+import { InfiniteData } from '@tanstack/react-query';
+import { getFollowingAlbums } from '../../services/album/album';
+import { useEffect } from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { AlbumListResponse, MyAlbumListResponse } from '../../types/api/album';
+const useGetMyAlbums = () => {
+    const { ref, inView } = useInView({ delay: 100, rootMargin: '100px' });
+    const { isLoading, data, isError, error, hasNextPage, fetchNextPage, isFetchingNextPage } = useInfiniteQuery<
+        MyAlbumListResponse,
+        Error,
+        InfiniteData<MyAlbumListResponse>,
+        [string, string],
+        number
+    >({
+        queryKey: ['album', 'following'],
+        queryFn: getFollowingAlbums,
+        initialPageParam: 0,
+        getNextPageParam: (lastPage) => {
+            if (!lastPage.next) return undefined;
+            const url = new URL(lastPage.next);
+            const nextOffset = url.searchParams.get('offset');
+            console.log(url);
+            return Number(nextOffset);
+        },
+        staleTime: 5 * 60 * 1000,
+        gcTime: 6 * 60 * 1000,
+    });
+
+    useEffect(() => {
+        if (inView && hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+        }
+    }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+    return {
+        data,
+        isLoading,
+        error,
+        isError,
+        ref,
+        isFetchingNextPage,
+    };
+};
+export default useGetMyAlbums;
---
+++ client/src/hooks/playlist/useGetPlaylists.ts
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import getPlaylists from '../../services/playlist/playlist';
+import { PlaylistListResponse } from '../../types/api/playlist';
+import { InfiniteData } from '@tanstack/react-query';
+
+export const useGetPlaylists = () => {
+    const { ref, inView } = useInView({ delay: 100, rootMargin: '100px' });
+    const { data, isLoading, isError, hasNextPage, fetchNextPage, isFetchingNextPage } = useInfiniteQuery<
+        PlaylistListResponse,
+        Error,
+        InfiniteData<PlaylistListResponse>,
+        [string],
+        number
+    >({
+        queryKey: ['playlists'],
+        queryFn: getPlaylists,
+        initialPageParam: 0,
+        getNextPageParam: (lastPage) => {
+            if (!lastPage.next) return undefined;
+            const url = new URL(lastPage.next);
+            const nextOffset = url.searchParams.get('offset');
+            return Number(nextOffset);
+        },
+        staleTime: 5000,
+    });
+    useEffect(() => {
+        if (inView && hasNextPage && !isFetchingNextPage) {
+            fetchNextPage();
+        }
+    }, [inView, hasNextPage, isFetchingNextPage, fetchNextPage]);
+
+    return { ref, data, isLoading, isError, isFetchingNextPage };
+};
