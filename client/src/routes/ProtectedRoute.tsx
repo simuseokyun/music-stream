aa0eeefb7e3e12b2affc322bcaea9e5fde5b1be1
simuseokyun <sim31059999@gmail.com>
2025-06-28 19:46:56 +0900

    Feat : 클라이언트 측 라우터 미들웨어

---
+++ client/src/routes/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+import { Navigate } from 'react-router-dom';
+import useUserStore from '../store/user';
+
+export default function ProtectedRoute({ children }: { children: ReactNode }) {
+    const { user, hydrated } = useUserStore();
+    if (!hydrated) {
+        return (
+            <div className="flex-1 flex justify-center items-center">
+                <img className="img-medium m-20 animate-spin" src="/assets/loading.png" alt="Loading" />
+            </div>
+        );
+    }
+    if (!user) {
+        return <Navigate to="/login" replace />;
+    }
+    return <>{children}</>;
+}
