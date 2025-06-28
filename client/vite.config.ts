d0dd439799947191d00ef2cfd33e6390a673a08f
simuseokyun <sim31059999@gmail.com>
2025-06-28 19:33:32 +0900

    Feat : 프록시 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+    },
     server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+            },
+        },
         port: 3000,
     },
     base: '/',
