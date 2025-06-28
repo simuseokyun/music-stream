213d3ad5b06e5694c15f6048c8c9f2f7b1b7f945
simuseokyun <sim31059999@gmail.com>
2025-06-28 19:38:21 +0900

    Feat : 플레이리스트 생성 로직

--- client/src/hooks/playlist/useCreatePlaylist.ts
+++ client/src/hooks/playlist/useCreatePlaylist.ts
@@ -1,18 +1,20 @@
 import { useMutation, useQueryClient, InfiniteData } from '@tanstack/react-query';
-import { fetchWithAuth } from '../../services/api/client';
+import { axiosWithAuth } from '../../services/api/client';
 import { PlaylistListResponse } from '../../types/api/playlist';
-
+import useThrottledToast from '../common/useTrottledToast';
 const useCreatePlaylist = () => {
     const queryClient = useQueryClient();
+    const toast = useThrottledToast();
     return useMutation({
         mutationFn: async (data: { name: string; description: string; user: string }) => {
-            return await fetchWithAuth(`/api/me/playlist/add`, {
+            return await axiosWithAuth(`/api/me/playlist/add`, {
                 method: 'post',
-                headers: { 'Content-Type': 'application/json' },
                 data,
+                headers: { 'Content-Type': 'application/json' },
             });
         },
         onSuccess: (data) => {
+            toast('success', '플레이리스트를 생성하였습니다', data.user);
             const cache = queryClient.getQueryData<InfiniteData<PlaylistListResponse>>(['playlists']);
             if (!cache) {
                 return;
@@ -23,8 +25,8 @@ const useCreatePlaylist = () => {
             };
             queryClient.setQueryData(['playlists'], newData);
         },
-        onError: (error) => {
-            console.log(error);
+        onError: (error, variables) => {
+            toast('error', '플레이리스트 생성에 실패했습니다', variables.user);
         },
     });
 };
