5d526938a408eb5cef30eae8cfce7cac5f0fcc40
simuseokyun <sim31059999@gmail.com>
2025-06-28 19:29:01 +0900

    Feat : 아티스트 바인딩 함수 추가

---
+++ client/src/components/common/Artists.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+export default function Artists({ artists }: { artists: { name: string; id: string }[] }) {
+    return (
+        <div className="mt-0.5 text-ellipsis">
+            {artists?.map((artist, i) => {
+                return (
+                    <Link
+                        className="text-sm text-sub pointer-events-none md:pointer-events-auto"
+                        to={`/artist/${artist?.id}`}
+                        key={artist?.id}
+                    >
+                        <span>
+                            {artist?.name}
+                            {artists?.length == 1 ? undefined : artists[i + 1] ? ' , ' : undefined}
+                        </span>
+                    </Link>
+                );
+            })}
+        </div>
+    );
+}
