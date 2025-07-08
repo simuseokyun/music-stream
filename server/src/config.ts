44fef9fb445197e070852501e1b35596e51270b8
simuseokyun <sim31059999@gmail.com>
2025-07-08 21:00:09 +0900

    Refactor : 변수 명 변경

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,5 +1,6 @@
-export const client_id = process.env.SPOTIFY_CLIENT_ID || '';
-export const client_secret = process.env.SPOTIFY_CLIENT_SECRET || '';
-export const redirect_uri = process.env.REDIRECT_URI || '';
-export const baseUrl = process.env.BASE_URL || '';
-export const basicAuth = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
+export const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || '';
+export const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || '';
+export const REDIRECT_URI = process.env.REDIRECT_URI || '';
+export const BASE_URL_AUTH = process.env.BASE_URL_AUTH || '';
+export const BASE_URL_API = process.env.BASE_URL_API || '';
+export const BASIC_AUTH = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
