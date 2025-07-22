b31480e861049503597cf25fe41c8f8dbe55dd22
simuseokyun <sim31059999@gmail.com>
2025-07-22 10:36:24 +0900

    Feat : 아티스트 목록 불러오기

--- client/src/hooks/album/useArtistAlbums.ts
+++ client/src/hooks/album/useArtistAlbums.ts
@@ -1,17 +1,17 @@
-import { getLocalStorage } from '../../utils/common/setLocalStorage';
 import { useQuery } from '@tanstack/react-query';
+import { getArtistAlbums } from '../../services/album/album';
 import { AlbumListResponse } from '../../types/api/album';
-import { getArtistAlbum } from '../../services/album/album';
 
 const useGetArtistAlbums = (artistId?: string) => {
     const { data, isLoading, isError } = useQuery<AlbumListResponse>({
-        queryKey: ['artistAlbum', artistId],
+        queryKey: ['album', 'artist', artistId],
         queryFn: async () => {
-            if (!artistId) Promise.reject('아티스트 아이디 필요');
-
-            return getArtistAlbum(artistId!);
+            if (!artistId) throw new Error('아티스트 아이디가 필요합니다');
+            return getArtistAlbums(artistId);
         },
         enabled: !!artistId,
+        staleTime: Infinity,
+        gcTime: Infinity,
     });
     return { data, isLoading, isError };
 };
