58d9193382fbc35b09007b5d612a93ad407c4e0f
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:14:03 +0900

    Refactor : 스타일 변경 및 props추가

--- client/src/components/library/MyAlbumItem.tsx
+++ client/src/components/library/MyAlbumItem.tsx
@@ -1,28 +1,30 @@
 import { useNavigate } from 'react-router-dom';
-import { MyAlbumItem as MyAlbumItemProps } from '../../types/api/album';
+import { Album, AlbumType } from '../../types/models/album';
 import { useSortTabStore } from '../../store/library';
 
-export default function MyAlbumItem({ album }: MyAlbumItemProps) {
-    const { id, name, artists, images } = album;
+export default function AlbumItem({ album }: { album: Album }) {
+    const { id, name, artists, images, album_type } = album;
     const navigate = useNavigate();
     const sortState = useSortTabStore((state) => state.active);
-
     const goAlbum = () => {
         navigate(`/album/${id}`);
     };
     return (
         <li
-            className={`w-full p-[10px] rounded-md cursor-pointer ${sortState === 'flex' && 'w-full flex items-center'} md:hover:bg-[#1a191a]`}
+            className={`w-full p-2 rounded-md ${sortState === 'flex' && 'w-full flex items-center'} md:p-3 md:hover:bg-[#1a191a]`}
             onClick={goAlbum}
         >
             <img
-                className={`rounded-md ${sortState === 'flex' ? 'w-[75px] h-[75px] flex-shrink-0' : ''}`}
-                src={images[0].url}
-                alt="앨범커버"
+                className={`rounded-md ${sortState === 'flex' ? 'w-[75px] h-[75px]' : 'w-full aspect-square'}`}
+                src={images[0]?.url ?? '/assets/playlist.svg'}
+                alt="앨범 커버"
             />
-            <div className={`mt-2 ${sortState === 'flex' ? 'ml-4 flex-1 min-w-0' : ''}`}>
-                <h1 className="text-ellipsis text-sm font-bold md:text-base">{name}</h1>
-                <p className="text-ellipsis text-sm text-sub mt-0.5">{artists[0].name}</p>
+            <div className={`mt-2 ${sortState === 'flex' && 'ml-4 flex-1 min-w-0'}`}>
+                <h1 className="text-sm truncate md:text-base">{name}</h1>
+                <p className="text-sm text-sub truncate">
+                    {artists[0]?.name}
+                    <span className="ml-2">{AlbumType[album_type as keyof typeof AlbumType]}</span>
+                </p>
             </div>
         </li>
     );
