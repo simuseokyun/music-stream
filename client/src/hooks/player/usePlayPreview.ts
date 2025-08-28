9323a18c6e29ac7f6ba21c17547c9b2b6f5c7d4f
simuseokyun <sim31059999@gmail.com>
2025-08-28 15:10:54 +0900

    Refactor : 전역상태 구독 항목 추가

--- client/src/hooks/player/usePlayPreview.ts
+++ client/src/hooks/player/usePlayPreview.ts
@@ -1,20 +1,27 @@
 import axios from 'axios';
-import { usePlayerStore } from '../../store/player';
-import { TrackToPlay } from '../../types/models/player.';
 import useThrottledToast from '../common/useTrottledToast';
+import { PlayState } from '../../types/models/player.';
+import { usePlayerStore } from '../../store/player';
+import { usePlayerKey } from '../../store/common';
 
 const usePlayPreview = () => {
     const toast = useThrottledToast();
-    const { setState } = usePlayerStore();
-    const playPreview = async ({ id, title, artist, image }: TrackToPlay) => {
+    const { url, setPlayerState } = usePlayerStore();
+    const { key, setKey } = usePlayerKey();
+    const playPreview = async ({ id, title, artist, image }: PlayState) => {
         try {
             const response = await axios.post(`/api/preview?title=${encodeURIComponent(title)}`, { id });
             const data = response.data;
-            setState({ title, artist, url: data[0].previewUrls[0], image });
-            return { success: true, data: data, image };
-        } catch (error) {
-            toast('error', `해당 곡은 재생할 수 없습니다. 다음 트랙으로 넘어갑니다`, id);
-            return { success: false, error };
+            if (url === data[0]?.previewUrls[0]) {
+                setKey(key + 1);
+            } else {
+                setKey(0);
+            }
+            setPlayerState({ title, artist, url: data[0].previewUrls[0], image });
+            return { playState: true, message: '재생 성공' };
+        } catch {
+            toast('error', `${title} : 해당 곡은 재생할 수 없습니다. 다음 트랙으로 넘어갑니다`);
+            return { playState: false, message: '재생 실패' };
         }
     };
     return { playPreview };
