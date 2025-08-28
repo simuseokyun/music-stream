823953c55cb95d883fe91a5630321210cbb7e1f7
simuseokyun <sim31059999@gmail.com>
2025-08-28 13:50:52 +0900

    refactor :스타일 변경

--- client/src/components/allAlbum/AlbumItem.tsx
+++ client/src/components/allAlbum/AlbumItem.tsx
@@ -1,24 +1,19 @@
 import { useNavigate } from 'react-router-dom';
-import { ArtistAlbumItem } from '../../types/models/album';
-import { AlbumType } from '../../types/models/album';
-export default function AlbumItem({ album: { id, name, images, album_type, release_date } }: ArtistAlbumItem) {
+import { AlbumType, Album } from '../../types/models/album';
+export default function AlbumItem({ album }: { album: Album }) {
+    const { id, name, images, release_date, album_type } = album;
     const navigate = useNavigate();
     const goAlbum = () => {
         navigate(`/album/${id}`);
     };
 
     return (
-        <li
-            className="w-full rounded-md p-2 md:hover:bg-[#1a191a] max-[768px]:p-[5px] md:hover:bg-unset"
-            onClick={goAlbum}
-        >
-            <img className="rounded-md" src={images[0].url} alt="앨범 커버" />
-            <h1 className="text-ellipsis mt-2 font-semibold text-sm md:text-base">{name}</h1>
-            <div className="mt-1">
+        <li className="w-full rounded-md p-2 md:p-3 md:hover:bg-[#1a191a] " onClick={goAlbum}>
+            <img className="rounded-md" src={images[0]?.url ?? '/assets/playlist.svg'} alt="앨범 커버" />
+            <h1 className="text-sm font-semibold mt-2 truncate md:text-base">{name}</h1>
+            <div>
                 <span className="text-sub text-sm">{release_date.slice(0, 4)}</span>
-                <span className="text-sub text-sm ml-1">
-                    {album_type === 'album' ? AlbumType.album : AlbumType.single}
-                </span>
+                <span className="text-sub text-sm ml-1">{AlbumType[album_type as keyof typeof AlbumType]}</span>
             </div>
         </li>
     );
--- client/src/components/artist/AlbumItem.tsx
+++ client/src/components/artist/AlbumItem.tsx
@@ -1,20 +1,18 @@
 import { useNavigate } from 'react-router-dom';
-import { ArtistAlbumItem } from '../../types/models/album';
+import { Album } from '../../types/models/album';
 import { AlbumType } from '../../types/models/album';
 
-export default function AlbumItem({ album }: ArtistAlbumItem) {
+export default function AlbumItem({ album }: { album: Album }) {
     const { id, images, name, release_date, album_type } = album;
     const navigate = useNavigate();
     const goAlbum = () => navigate(`/album/${id}`);
     return (
-        <li className="md:hover:bg-[#1a191a] p-[10px] rounded-md cursor-pointer" onClick={goAlbum}>
-            <img className="rounded-md" src={images[0]?.url} alt="앨범 커버" />
-            <h1 className="text-ellipsis mt-2 font-semibold text-sm sm:text-base">{name}</h1>
-            <div className="mt-1">
+        <li className="p-3 rounded-md md:hover:bg-[#1a191a] " onClick={goAlbum}>
+            <img className="w-full rounded-md" src={images[0]?.url ?? '/assets/playlist.svg'} alt="앨범 커버" />
+            <h1 className="text-sm font-semibold mt-2  truncate md:text-base">{name}</h1>
+            <div>
                 <span className="text-sub text-sm">{release_date.slice(0, 4)}</span>
-                <span className="text-sub text-sm ml-1">
-                    {album_type === 'album' ? AlbumType.album : AlbumType.single}
-                </span>
+                <span className="text-sub text-sm ml-1">{AlbumType[album_type as keyof typeof AlbumType]}</span>
             </div>
         </li>
     );
