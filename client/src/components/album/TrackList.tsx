9e43e8eadbf44727db2f58d8f634f5693a5f89e8
simuseokyun <sim31059999@gmail.com>
2025-08-28 13:50:32 +0900

    refactor : sinkError 함수 제거 및 옵셔널체이닝

--- client/src/components/album/TrackList.tsx
+++ client/src/components/album/TrackList.tsx
@@ -1,45 +1,33 @@
 import TrackItem from './TrackItem';
 import useThrottledToast from '../../hooks/common/useTrottledToast';
-import usePlayPreview from '../../hooks/player/usePlayPreview';
-import { TrackToPlay } from '../../types/models/player.';
 import usePlayThrottle from '../../hooks/player/usePlayThrottle';
-import { AlbumDataResponse } from '../../types/api/album';
-import { useCurrentPlaylistStore } from '../../store/player';
+import usePlayTrack from '../../hooks/player/usePlayTrack';
+import { AlbumInfoResponse } from '../../types/api/album';
 
-export default function TrackList({ data }: { data: AlbumDataResponse }) {
-    const { setPlaylist, setIndex } = useCurrentPlaylistStore();
+export default function TrackList({ data }: { data: AlbumInfoResponse }) {
+    const { playTrack } = usePlayTrack();
     const toast = useThrottledToast();
-    const { playPreview } = usePlayPreview();
-    const onPlay = usePlayThrottle(async ({ id, title, artist, image }: TrackToPlay) => {
-        const list = data.tracks.items.length ? data.tracks?.items : null;
-        if (!list) return toast('error', '재생할 곡이 없습니다', id);
+    const onPlay = usePlayThrottle(async ({ id }: { id: string }) => {
+        const list = data?.tracks?.items ?? [];
+        if (!list) {
+            toast('error', '곡을 재생할 수 없습니다');
+            return;
+        }
         const index = list.findIndex((item) => item.id === id);
         const newList = list.map(({ id, name }) => ({
             id,
             title: name,
-            artist: data?.artists[0].name,
+            artist: data?.artists[0]?.name,
             image: data?.images[0]?.url,
         }));
-        await playTrackAndSkipOnError(index, newList);
+        await playTrack(index, newList);
     });
-    const playTrackAndSkipOnError = async (index: number, list: TrackToPlay[]) => {
-        try {
-            const result = await playPreview(list[index]);
-            if (result.success) {
-                setIndex(index);
-                setPlaylist([...list]);
-            }
-            if (!result.success) {
-                playTrackAndSkipOnError(index + 1, list);
-            }
-        } catch {}
-    };
 
     return (
-        <table className="w-table-auto w-full table-fixed mt-2">
+        <table className="w-full table-fixed mt-2">
             <tbody>
                 {data?.tracks?.items.map((track) => (
-                    <TrackItem key={track.id} track={track} image={data.images[0].url} onPlay={onPlay} />
+                    <TrackItem key={track.id} track={track} image={data?.images[0]?.url} onPlay={onPlay} />
                 ))}
             </tbody>
         </table>
