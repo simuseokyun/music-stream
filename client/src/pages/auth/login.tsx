b7600e4062725c526467308a8c17c70b07987601
simuseokyun <sim31059999@gmail.com>
2025-08-28 15:12:42 +0900

    Refactor : 스타일 변경

--- client/src/pages/auth/login.tsx
+++ client/src/pages/auth/login.tsx
@@ -1,11 +1,11 @@
-import onLogin from '../../utils/api/login';
+import onLogin from '../../utils/auth/spotify';
 
 export default function Login() {
     return (
-        <div className="flex-1 relative rounded-lg">
+        <div className="flex-1">
             <div className="text-center mt-20">
-                <h1 className="item-center text-2xl mb-5 font-bold">로그인 후 이용할 수 있습니다</h1>
-                <button className="bg-accent font-black py-[10px] px-[15px] text-bold rounded-4xl" onClick={onLogin}>
+                <h1 className="text-2xl font-bold item-center ">로그인 후 이용해주세요</h1>
+                <button className="bg-accent text-white font-bold mt-4 p-3 rounded-3xl" onClick={onLogin}>
                     로그인하기
                 </button>
             </div>
