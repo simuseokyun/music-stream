0da9852da0addaf8c7fa3fc0270f35ae0b5eb342
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:14:57 +0900

    Refactor : 스타일 변경 Props변경

--- client/src/components/player/Player.tsx
+++ client/src/components/player/Player.tsx
@@ -1,39 +1,32 @@
-import { usePlayerStore } from '../../store/player';
 import ActionButtons from './ActionButtons';
 import ProgressBar from './ProgressBar';
 import usePlayerControl from '../../hooks/player/usePlayerControl';
+import { usePlayerStore } from '../../store/player';
+
 export default function Player() {
     const { isPlaying, title, url, artist, image } = usePlayerStore();
-    const { playerControl, onNext, onPrev } = usePlayerControl();
-
+    const { onToggle, onNext, onClickNext, onPrev } = usePlayerControl();
     return (
         <div
-            className={`w-full bg-[#1a191a] fixed ${!!url ? 'md:bottom-0 bottom-[50px]' : 'bottom-[-100px]'} transition-all duration-600 md:border-t-1 md:bg-main md:border-[#e2e2e2] `}
+            className={`fixed w-full bg-[#1a191a] transition-all duration-300 ${!!url ? ' bottom-[50px] md:bottom-0' : 'bottom-[-100px]'} md:border-t-1 md:bg-main`}
         >
-            <div className="max-w-[1200px] p-[10px] mx-auto">
-                <div className="flex items-center">
-                    <div className="flex items-center flex-1 min-w-0">
-                        <img
-                            className="w-[40px] h-[40px] md:w-[60px] md:h-[60px] rounded-md shrink-0"
-                            src={image || '/assets/playlist.svg'}
-                            alt="앨범커버"
-                        />
-                        <div className="ml-2 flex-1 min-w-0">
-                            <h1 className="truncate text-md md:text-base font-semibold">{title}</h1>
-                            <p className="truncate text-[12px] text-sub">{artist}</p>
-                        </div>
+            <div className="w-full flex items-center p-2">
+                <div className="flex items-center flex-1 min-w-0">
+                    <img
+                        className="w-[40px] h-[40px] md:img-medium  rounded-md shrink-0"
+                        src={image ?? '/assets/playlist.svg'}
+                        alt="앨범 커버"
+                    />
+                    <div className=" flex-1 min-w-0 ml-2">
+                        <h1 className="text-sm font-semibold truncate md:text-base ">{title}</h1>
+                        <p className="text-sm text-sub truncate ">{artist}</p>
                     </div>
-                    <div className="flex-1">
-                        <ActionButtons
-                            isPlaying={isPlaying}
-                            playerControl={playerControl}
-                            onNext={onNext}
-                            onPrev={onPrev}
-                        />
-                        <ProgressBar url={url} isPlaying={isPlaying} onPause={playerControl} onNext={onNext} />
-                    </div>
-                    <div className="md:flex-1" />
                 </div>
+                <div className="flex-1">
+                    <ActionButtons isPlaying={isPlaying} onToggle={onToggle} onNext={onClickNext} onPrev={onPrev} />
+                    <ProgressBar isPlaying={isPlaying} onNext={onNext} url={url} />
+                </div>
+                <div className="md:flex-1" />
             </div>
         </div>
     );
