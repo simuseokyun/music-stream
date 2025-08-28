9c564988552e52bfc0a44f1cfa94a521bba2f012
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:17:19 +0900

    Refactor : 팔로우/언팔로우 로직 합치기

--- client/src/components/searchResult/FollowTab.tsx
+++ client/src/components/searchResult/FollowTab.tsx
@@ -1,25 +1,25 @@
 import { MouseEventHandler } from 'react';
 import { useArtistFollow } from '../../hooks/artist/useArtistFollow';
-import { Artist } from '../../types/api/artist';
+import { Artist } from '../../types/models/artist';
 
 export default function FollowTab({ artist }: { artist: Artist }) {
     const { isLoading, followState, follow, unFollow } = useArtistFollow(artist);
-    const onFollow: MouseEventHandler = (e) => {
+    const onFollowToggle: MouseEventHandler<HTMLButtonElement> = (e) => {
         e.stopPropagation();
-        follow();
-    };
-    const onUnfollow: MouseEventHandler = (e) => {
-        e.stopPropagation();
-        unFollow();
+        if (followState) {
+            unFollow();
+        } else {
+            follow();
+        }
     };
     if (isLoading) {
         return null;
     }
     return (
-        <div className="mt-2">
+        <div className="mt-1">
             <button
-                className={`transition text-sm font-bold  py-1 px-3  rounded-2xl text-black ${followState ? 'bg-white' : 'bg-accent'}`}
-                onClick={followState ? onUnfollow : onFollow}
+                className={`text-sm text-black font-bold  py-1 px-3  rounded-2xl  transition  ${followState ? 'bg-white' : 'bg-accent'}`}
+                onClick={onFollowToggle}
             >
                 {followState ? '팔로우 중' : '팔로우'}
             </button>
