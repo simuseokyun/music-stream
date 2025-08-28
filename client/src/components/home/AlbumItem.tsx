075c9cf6a20c7b6d1114cb3f67eb3d93f5ed9014
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:11:46 +0900

    Refactor : 컴포넌트명 및 스타일 변경

--- client/src/components/home/AlbumItem.tsx
+++ client/src/components/home/AlbumItem.tsx
@@ -1,16 +1,17 @@
 import { useNavigate } from 'react-router-dom';
-import { NewAlbumItem as INewAlbumItem } from '../../types/models/album';
+import { Album } from '../../types/models/album';
 
-export default function NewAlbumItem({ id, name, artist, cover }: INewAlbumItem) {
+export default function AlbumItem({ album }: { album: Album }) {
+    const { id, name, artists, images } = album;
     const navigate = useNavigate();
     const goAlbum = () => {
         navigate(`/album/${id}`);
     };
     return (
-        <div onClick={goAlbum} className=" p-[5px] rounded-md cursor-pointer md:hover:bg-[#1a191a] md:p-[10px]">
-            <img className="rounded-md" src={cover} alt="앨범커버" />
-            <h1 className="text-ellipsis text-sm font-bold md:text-base mt-2 ">{name}</h1>
-            <p className="artists text-ellipsis text-sm text-sub">{artist}</p>
+        <div onClick={goAlbum} className="p-2  rounded-md md:hover:bg-[#1a191a] md:p-3">
+            <img className="rounded-md" src={images[0]?.url ?? '/assets/playlist.svg'} alt="앨범 커버" />
+            <h1 className="text-sm md:text-base mt-2 truncate">{name}</h1>
+            <p className=" text-sm text-sub truncate">{artists[0]?.name}</p>
         </div>
     );
 }
