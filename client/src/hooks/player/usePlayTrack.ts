e43ce7ab220513a93cbcc088ed18090a57f44949
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:26:05 +0900

    Feat : 트랙 재생 로직 추가

---
+++ client/src/hooks/player/usePlayTrack.ts
@@ -0,0 +1,25 @@
+import usePlayPreview from './usePlayPreview';
+import useThrottledToast from '../common/useTrottledToast';
+import { PlayState } from '../../types/models/player.';
+import { useCurrentPlaylistStore } from '../../store/player';
+
+const usePlayTrack = () => {
+    const { playPreview } = usePlayPreview();
+    const { setPlaylist, setIndex } = useCurrentPlaylistStore();
+    const toast = useThrottledToast();
+    const playTrack = async (index: number, list: PlayState[]) => {
+        try {
+            const result = await playPreview(list[index]);
+            if (result.playState) {
+                setIndex(index);
+                setPlaylist([...list]);
+            } else {
+                await playTrack(index + 1, list);
+            }
+        } catch {
+            toast('error', '곡 재생에 실패했습니다');
+        }
+    };
+    return { playTrack };
+};
+export default usePlayTrack;
