6ab9bb77531dedda70a2ec2b9c7dc5c29cfedf21
simuseokyun <sim31059999@gmail.com>
2025-08-28 13:47:04 +0900

    refactor : 스타일 변경 및 함수 재설정

--- client/src/components/album/AlbumInfo.tsx
+++ client/src/components/album/AlbumInfo.tsx
@@ -4,22 +4,27 @@ import { AlbumType } from '../../types/models/album';
 import useUserStore from '../../store/user';
 import useAlbumStore from '../../store/album';
 export default function AlbumInfo() {
-    const { user } = useUserStore();
-    const { id, name, artist_name, image, type, track_length, artist_id } = useAlbumStore();
-
+    const session = useUserStore((state) => state.user);
+    const { id, name, artistName, image, type, trackLength, artistId } = useAlbumStore();
+    const tabProps = { id, name, artistId, artistName, image };
     return (
-        <div className="text-center md:flex md:items-end md:text-start border-b-1 border-white/20 pb-[20px]">
-            <img className="inline-block w-[150px] rounded-[8px] md:w-[200px] " src={image} />
+        <div className="text-center border-b border-white/20 pb-5 md:flex md:items-end md:text-start ">
+            <img className="w-[150px] rounded-md md:w-[200px]" src={image} alt="앨범 커버" />
             <div className="md:ml-4">
-                <span className="mb-2 hidden md:block">{AlbumType[type as keyof typeof AlbumType] ?? ''}</span>
-                <h1 className="text-[24px] mt-2 my-[4px] font-bold  md:text-[30px] md:mt-0">{name}</h1>
-                <div className="mb-2">
-                    <Link className="md:hover:underline text-sub" to={`/artist/${artist_id}`}>
-                        {artist_name}
+                <span className="mb-2 hidden md:block">{AlbumType[type as keyof typeof AlbumType]}</span>
+                <h1
+                    className="text-2xl mt-4 font-bold md:text-[30px] md:mt-0 
+               line-clamp-2 break-words"
+                >
+                    {name}
+                </h1>
+                <div className="my-1">
+                    <Link className="md:hover:underline text-sub" to={`/artist/${artistId}`}>
+                        {artistName}
                     </Link>
-                    <span className="text-sm mt-2 hidden md:block">{track_length}곡</span>
+                    <span className="text-sm text-sub ml-2">{trackLength}곡</span>
                 </div>
-                {user && <AlbumTab {...{ id, name, artist_name, artist_id, image, type }} />}
+                {session && <AlbumTab data={tabProps} />}
             </div>
         </div>
     );
--- client/src/components/album/AlbumTab.tsx
+++ client/src/components/album/AlbumTab.tsx
@@ -1,20 +1,17 @@
 import { useAlbumLike } from '../../hooks/album/useAlbumLike';
+import { AlbumTab as AlbumTabProps } from '../../types/models/album';
 
-import { AlbumTab as IAlbumTab } from '../../types/models/album';
-
-export default function AlbumTab({ id, name, artist_name, artist_id, image }: IAlbumTab) {
-    const { isLiked, addAlbum, deleteAlbum } = useAlbumLike(id, name, artist_name, artist_id, image);
+export default function AlbumTab({ data }: { data: AlbumTabProps }) {
+    const { id, name, artistId, artistName, image } = data;
+    const { isLiked, likedAlbum, unLikedAlbum } = useAlbumLike({ id, name, artistId, artistName, image });
     return (
         <div>
             {isLiked ? (
-                <button
-                    className="p-1 text-xs bg-sub text-black rounded-md border-1"
-                    onClick={() => deleteAlbum.mutate()}
-                >
+                <button className="p-1 text-xs bg-white text-black rounded-md border" onClick={() => unLikedAlbum()}>
                     찜 해제
                 </button>
             ) : (
-                <button className="p-1 text-xs bg-main rounded-md border-1" onClick={() => addAlbum.mutate()}>
+                <button className="p-1 text-xs bg-main rounded-md border" onClick={() => likedAlbum()}>
                     찜 하기
                 </button>
             )}
