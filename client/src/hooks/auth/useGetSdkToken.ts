b01ee2bdfafe6d7c4463857af766bc3c73a1649f
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:21:55 +0900

    Refactor : 쿼리명 변경

--- client/src/hooks/auth/useGetSdkToken.ts
+++ client/src/hooks/auth/useGetSdkToken.ts
@@ -1,16 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
-import { getSdkToken } from '../../services/auth/\bauth';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { getSdkToken } from '../../services/auth/auth';
 
 const useGetSdkToken = (code: string) => {
     const navigate = useNavigate();
     const { data, isLoading, isError, isSuccess } = useQuery({
-        queryKey: ['sdkToken'],
+        queryKey: ['token', 'sdk'],
         queryFn: () => getSdkToken(code),
         enabled: !!code,
         retry: 2,
     });
+
     useEffect(() => {
         if (data && isSuccess) {
             window.history.replaceState({}, document.title, window.location.pathname);
