133ee0c3e0225d48239c2f84c7f42b45e6033037
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:11:59 +0900

    Refactor : 변수명 변경

--- client/src/components/layout/MobileHeader.tsx
+++ client/src/components/layout/MobileHeader.tsx
@@ -1,21 +1,21 @@
 import { useNavigate } from 'react-router-dom';
-import onLogin from '../../utils/api/login';
 import useLogout from '../../hooks/auth/useLogout';
+import onLogin from '../../utils/auth/spotify';
 import useUserStore from '../../store/user';
 
 export default function MobileHeader() {
-    const user = useUserStore((state) => state.user);
     const { onLogout } = useLogout();
     const navigate = useNavigate();
+    const session = useUserStore((state) => state.user);
     const goback = () => navigate(-1);
 
     return (
-        <header className="w-full fixed top-0 left-0 px-4 py-4 flex bg-main items-center justify-between z-20 md:hidden">
-            <img className="img-small" src="/assets/backButton.svg" alt="뒤로가기" onClick={goback} />
-            {!user ? (
-                <img className="img-small" src="/assets/login.svg" alt="로그인" onClick={onLogin} />
+        <header className="w-full fixed top-0 left-0 p-4 flex bg-main items-center justify-between z-10 md:hidden">
+            <img className="img-small" src="/assets/backButton.svg" alt="이전 아이콘" onClick={goback} />
+            {!session ? (
+                <img className="img-small" src="/assets/login.svg" alt="로그인 아이콘" onClick={onLogin} />
             ) : (
-                <img className="img-small" src="/assets/logout.svg" alt="로그아읏" onClick={onLogout} />
+                <img className="img-small" src="/assets/logout.svg" alt="로그아웃 아이콘" onClick={onLogout} />
             )}
         </header>
     );
