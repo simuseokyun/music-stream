c3dec646983ceca391dff652990082b8a15410c3
simuseokyun <sim31059999@gmail.com>
2025-08-28 15:23:49 +0900

    Refactor : any타입 제거 및 useCallback 감싸기

--- client/src/hooks/player/usePlayThrottle.ts
+++ client/src/hooks/player/usePlayThrottle.ts
@@ -1,14 +1,17 @@
-import { useRef } from 'react';
-import { TrackToPlay } from '../../types/models/player.';
-function usePlayThrottle<T extends (...args: TrackToPlay[]) => any>(callback: T, delay: number = 1000) {
+import { useRef, useCallback } from 'react';
+
+function usePlayThrottle(onPlay: (args: { id: string }) => void | Promise<void>, delay = 1000) {
     const lastTime = useRef(0);
-    return (...args: Parameters<T>) => {
-        const now = Date.now();
-        if (now - lastTime.current > delay) {
-            callback(...args);
-            lastTime.current = now;
-        }
-    };
+    return useCallback(
+        (args: { id: string }) => {
+            const now = Date.now();
+            if (now - lastTime.current > delay) {
+                lastTime.current = now;
+                onPlay(args);
+            }
+        },
+        [onPlay, delay]
+    );
 }
 
 export default usePlayThrottle;
