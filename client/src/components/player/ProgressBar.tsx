afc423f2f24577503f8ddfbcb615f3992895796f
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:15:11 +0900

    Refactor : 같은 노래 클릭했을 때 key바꿔주기

--- client/src/components/player/ProgressBar.tsx
+++ client/src/components/player/ProgressBar.tsx
@@ -1,15 +1,26 @@
+import { useState, useRef, ChangeEventHandler } from 'react';
 import ReactPlayer from 'react-player';
-import { useState, useRef } from 'react';
 import { Player } from '../../types/models/player.';
+import { usePlayerKey } from '../../store/common';
 
-export default function ProgressBar({ url, isPlaying, onPause, onNext }: Player) {
+export default function ProgressBar({ url, isPlaying, onNext }: Player) {
     const [played, setPlayed] = useState(0);
     const playerRef = useRef<ReactPlayer>(null);
+    const { key } = usePlayerKey();
+    const onPregrss = ({ played }: { played: number }) => {
+        const value = Number(played.toFixed(2));
+        setPlayed(value);
+    };
+    const setProgress: ChangeEventHandler<HTMLInputElement> = (e) => {
+        const value = parseFloat(e.target.value);
+        setPlayed(value);
+        playerRef?.current?.seekTo(value);
+    };
 
     return (
         <div className="hidden md:block">
             <ReactPlayer
-                // key={url}
+                key={url + key}
                 url={url}
                 ref={playerRef}
                 width={0}
@@ -17,25 +28,21 @@ export default function ProgressBar({ url, isPlaying, onPause, onNext }: Player)
                 playing={isPlaying}
                 controls={false}
                 onEnded={onNext}
-                onProgress={({ played }) => setPlayed(played)}
+                onProgress={onPregrss}
             />
             <div className="relative w-full h-[2px] rounded-[10px] bg-[#2a292a] md:mt-2">
                 <div
-                    className="absolute top-0 left-0 h-full bg-white bg-rounded-[10px] pointer-events-none"
+                    className="absolute top-0 left-0 h-full bg-white rounded-[10px] "
                     style={{ width: `${played * 100}%` }}
                 />
                 <input
-                    className="absolute top-0 left-0 w-full  appearance-none cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-moz-range-thumb]:appearance-none"
+                    className="absolute top-0 left-0 w-full bg-transparent cursor-pointer"
                     type="range"
                     min="0"
                     max="0.999999"
                     step="any"
                     value={played}
-                    onChange={(e) => {
-                        const value = parseFloat(e.target.value);
-                        setPlayed(value);
-                        playerRef?.current?.seekTo(value);
-                    }}
+                    onChange={setProgress}
                 />
             </div>
         </div>
