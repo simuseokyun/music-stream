915e4a720d77139d9e37eef3815febf7dadb8da8
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:21:34 +0900

    Refactor : return값 타입 지정 및 쿼리명 변경

--- client/src/hooks/artist/useGetArtist.ts
+++ client/src/hooks/artist/useGetArtist.ts
@@ -1,14 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import { ArtistResponse } from '../../types/api/artist';
-import { getArtist } from '../../services/artist/artist';
-import { getLocalStorage } from '../../utils/common/setLocalStorage';
+import { getDataWithoutAuth } from '../../services/api/client';
 const useGetArtist = (artistId?: string) => {
-    const { isLoading, data, isError } = useQuery<ArtistResponse>({
-        queryKey: ['artistCover', artistId],
+    const { data, isLoading, isError } = useQuery<ArtistResponse>({
+        queryKey: ['artist', 'info', artistId],
         queryFn: async () => {
-            return getArtist(artistId!);
+            if (!artistId) throw new Error('아티스트 아이디가 필요합니다');
+            return getDataWithoutAuth<ArtistResponse>(`/v1/artists/${artistId}`);
         },
         enabled: !!artistId,
+        staleTime: Infinity,
+        gcTime: Infinity,
     });
     return { data, isLoading, isError };
 };
