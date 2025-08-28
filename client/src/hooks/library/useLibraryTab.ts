a02a360f9ce6bb142c32d1f1e43e445bf9cdf73b
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:24:04 +0900

    Refactor : 함수명 변경

--- client/src/hooks/library/useLibraryTab.ts
+++ client/src/hooks/library/useLibraryTab.ts
@@ -1,8 +1,8 @@
-import { useLibraryTabStore } from '../../store/library';
-import { useSearchParams } from 'react-router-dom';
-import { useModalStore } from '../../store/common';
 import { useEffect } from 'react';
-import { useSortTabStore } from '../../store/library';
+import { useSearchParams } from 'react-router-dom';
+import { useLibraryTabStore, useSortTabStore } from '../../store/library';
+import { useModalStore } from '../../store/common';
+import { UseSortTab, UseLibraryTab } from '../../types/store/store';
 
 const useLibraryTab = () => {
     const { active, setActive } = useLibraryTabStore();
@@ -11,25 +11,26 @@ const useLibraryTab = () => {
     const openModal = useModalStore((state) => state.open);
     const { active: sortState, setActive: setSotrActive } = useSortTabStore();
 
-    const onClickGridTab = (tab: 'grid' | 'flex') => {
+    const onClickSortTab = (tab: UseSortTab['active']) => {
         setSotrActive(tab);
     };
-    const onClickTab = (tab: 'album' | 'playlist') => {
+    const onClickLeftTab = (tab: UseLibraryTab['active']) => {
         setActive(tab);
         const newParams = new URLSearchParams(searchParams);
         newParams.delete('album');
         newParams.delete('playlist');
+        newParams.delete('artist');
         newParams.set('active', tab);
         setSearchParams(newParams);
     };
 
     useEffect(() => {
-        if (activeState === 'playlist' || activeState === 'album') {
+        if (activeState === 'playlist' || activeState === 'album' || activeState === 'artist') {
             setActive(activeState);
         } else {
             setActive('playlist');
         }
     }, [activeState]);
-    return { active, openModal, onClickGridTab, onClickTab, sortState, activeState };
+    return { active, openModal, onClickSortTab, onClickLeftTab, sortState, activeState };
 };
 export default useLibraryTab;
