691f4eb9140ee2383b96d7a766bb790df5513815
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:12:28 +0900

    Refactor : 스타일 변경

--- client/src/components/library/LibraryTab.tsx
+++ client/src/components/library/LibraryTab.tsx
@@ -1,31 +1,37 @@
 import useLibraryTab from '../../hooks/library/useLibraryTab';
 
 export default function LibraryTab() {
-    const { active, openModal, onClickGridTab, onClickTab, sortState, activeState } = useLibraryTab();
+    const { active, openModal, onClickLeftTab, onClickSortTab, sortState, activeState } = useLibraryTab();
     return (
         <div className="flex justify-between items-center">
             <div className="flex justify-between items-center">
                 <button
-                    className={`text-sm p-1 px-2 rounded-md ${active === 'playlist' ? 'bg-active' : ''}`}
-                    onClick={() => onClickTab('playlist')}
+                    className={`text-sm p-2 rounded-md ${active === 'playlist' && 'bg-active'}`}
+                    onClick={() => onClickLeftTab('playlist')}
                 >
                     플레이리스트
                 </button>
                 <button
-                    className={`text-sm p-1 px-2 rounded-md ${active === 'album' ? 'bg-active' : ''}`}
-                    onClick={() => onClickTab('album')}
+                    className={`text-sm p-2 rounded-md ${active === 'album' && 'bg-active'}`}
+                    onClick={() => onClickLeftTab('album')}
                 >
                     내가 찜한 앨범
+                </button>
+                <button
+                    className={`text-sm p-2 rounded-md ${active === 'artist' ? 'bg-active' : ''}`}
+                    onClick={() => onClickLeftTab('artist')}
+                >
+                    아티스트
                 </button>
             </div>
             <div className="flex items-center">
                 {activeState === 'playlist' && (
-                    <img className="p-1" src="/assets/addButton.svg" onClick={() => openModal('addPlaylist')} />
+                    <img className="p-2" src="/assets/addButton.svg" onClick={() => openModal('addPlaylist')} />
                 )}
                 {sortState === 'flex' ? (
-                    <img className="p-1" src="/assets/gridButton.svg" onClick={() => onClickGridTab('grid')} />
+                    <img className="p-2" src="/assets/gridButton.svg" onClick={() => onClickSortTab('grid')} />
                 ) : (
-                    <img className="p-1" src="/assets/listButton.svg" onClick={() => onClickGridTab('flex')} />
+                    <img className="p-2" src="/assets/listButton.svg" onClick={() => onClickSortTab('flex')} />
                 )}
             </div>
         </div>
