217214340a57c2d11bb2e4302f11aab396893a67
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:20:19 +0900

    Refactor : 쿼리명 변경

--- client/src/hooks/album/useArtistAlbums.ts
+++ client/src/hooks/album/useArtistAlbums.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import { getArtistAlbums } from '../../services/album/album';
 import { AlbumListResponse } from '../../types/api/album';
+import { getDataWithoutAuth } from '../../services/api/client';
 
 const useGetArtistAlbums = (artistId?: string) => {
     const { data, isLoading, isError } = useQuery<AlbumListResponse>({
-        queryKey: ['album', 'artist', artistId],
+        queryKey: ['artist', 'albums', artistId],
         queryFn: async () => {
             if (!artistId) throw new Error('아티스트 아이디가 필요합니다');
-            return getArtistAlbums(artistId);
+            return getDataWithoutAuth<AlbumListResponse>(artistId);
         },
         enabled: !!artistId,
         staleTime: Infinity,
