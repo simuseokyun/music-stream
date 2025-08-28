2d4806b70f652c11ac4e9c2518cc70c503cec366
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:26:22 +0900

    Feat : 뮤테이션 함수 로직 분리

--- client/src/hooks/playlist/useCreatePlaylist.ts
+++ client/src/hooks/playlist/useCreatePlaylist.ts
@@ -1,32 +1,25 @@
 import { useMutation, useQueryClient, InfiniteData } from '@tanstack/react-query';
-import { axiosWithAuth } from '../../services/api/client';
+import { createPlaylist } from '../../services/playlist/playlist';
 import { PlaylistListResponse } from '../../types/api/playlist';
 import useThrottledToast from '../common/useTrottledToast';
 const useCreatePlaylist = () => {
     const queryClient = useQueryClient();
     const toast = useThrottledToast();
     return useMutation({
-        mutationFn: async (data: { name: string; description: string; user: string }) => {
-            return await axiosWithAuth(`/api/me/playlist/add`, {
-                method: 'post',
-                data,
-                headers: { 'Content-Type': 'application/json' },
-            });
-        },
+        mutationFn: createPlaylist,
         onSuccess: (data) => {
-            toast('success', '플레이리스트를 생성하였습니다', data.user);
+            toast('success', '플레이리스트를 생성하였습니다');
             const cache = queryClient.getQueryData<InfiniteData<PlaylistListResponse>>(['playlists']);
-            if (!cache) {
-                return;
+            if (cache) {
+                const newData = {
+                    ...cache,
+                    pages: [{ ...cache.pages[0], items: [data, ...cache.pages[0].items] }, ...cache.pages.slice(1)],
+                };
+                queryClient.setQueryData(['playlists'], newData);
             }
-            const newData = {
-                ...cache,
-                pages: [{ ...cache.pages[0], items: [data, ...cache.pages[0].items] }, ...cache.pages.slice(1)],
-            };
-            queryClient.setQueryData(['playlists'], newData);
         },
-        onError: (error, variables) => {
-            toast('error', '플레이리스트 생성에 실패했습니다', variables.user);
+        onError: () => {
+            toast('error', '플레이리스트 생성에 실패했습니다');
         },
     });
 };
