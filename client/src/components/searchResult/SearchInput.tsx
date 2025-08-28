dd2781ee44163021c3978fa0cc2cee47f5784546
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:19:58 +0900

    Refactor : url 인코딩 및 스타일 변경

--- client/src/components/searchResult/SearchInput.tsx
+++ client/src/components/searchResult/SearchInput.tsx
@@ -1,19 +1,19 @@
-import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export const SearchInput = () => {
-    const navigate = useNavigate();
     const { register, setValue, handleSubmit } = useForm<{ title: string }>();
+    const navigate = useNavigate();
     const goSearchResult = ({ title }: { title: string }) => {
         setValue('title', '');
-        navigate(`/search?q=${title}`);
+        navigate(`/search?q=${encodeURIComponent(title)}`);
     };
 
     return (
-        <div className="w-full max-w-[400px] mx-auto bg-[rgba(0,0,0,0.5)] pb-6 pt-2  z-[10]">
-            <form className="text-center" onSubmit={handleSubmit(goSearchResult)}>
+        <div className="w-full flex flex-col items-center bg-[rgba(0,0,0,0.5)] pb-6 pt-2 z-10">
+            <form className="text-center w-full max-w-[400px]" onSubmit={handleSubmit(goSearchResult)}>
                 <input
-                    className="w-full p-2 rounded-[20px] text-white bg-[rgb(40,40,40)] border border-transparent text-sm"
+                    className="w-full p-2 rounded-3xl text-white bg-[rgb(40,40,40)] text-sm"
                     type="text"
                     placeholder="아티스트나 노래제목을 입력해주세요"
                     {...register('title', {
