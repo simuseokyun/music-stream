d5395ff2a515fa75651d93ea23237301ee42e097
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:08:51 +0900

    Refactor : 스타일 변경

--- client/src/components/common/Error.tsx
+++ client/src/components/common/Error.tsx
@@ -1,6 +1,6 @@
 export default function Error() {
     return (
-        <div className="p-[20px]">
+        <div className="p-10">
             <h1 className="text-6xl">404 : Not found</h1>
         </div>
     );
--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     return (
-        <div className="flex justify-center w-full">
-            <img className="img-medium m-20 animate-spin " src="/assets/loading.png" alt="Loading" />
+        <div className="flex justify-center w-full my-20">
+            <img className="img-medium m-15 animate-spin" src="/assets/loading.png" alt="로딩 아이콘" />
         </div>
     );
 }
