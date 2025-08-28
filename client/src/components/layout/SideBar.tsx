3195307a1699f5191332100069d4df38dfc9ae23
simuseokyun <sim31059999@gmail.com>
2025-08-28 14:12:14 +0900

    Refactor : 네비게이션 커스텀 훅 분리

--- client/src/components/layout/SideBar.tsx
+++ client/src/components/layout/SideBar.tsx
@@ -1,36 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import useLogout from '../../hooks/auth/useLogout';
-import onLogin from '../../utils/api/login';
 import NavItem from './NavItem';
+import useLogout from '../../hooks/auth/useLogout';
+import useSidebar from '../../hooks/common/useSideBar';
+import onLogin from '../../utils/auth/spotify';
 import useUserStore from '../../store/user';
 export default function SideBar() {
     const { onLogout } = useLogout();
-    const [activeMenu, setActiveMenu] = useState<'home' | 'library' | null>(null);
-    const location = useLocation();
-    const user = useUserStore((state) => state.user);
-    const navigate = useNavigate();
-
-    const goHome = () => {
-        navigate('/home');
-    };
-    const goLibrary = () => {
-        navigate('/library?active=playlist');
-    };
-    const goBack = () => {
-        navigate(-1);
-    };
-
-    useEffect(() => {
-        if (location.pathname === '/home') {
-            setActiveMenu('home');
-        } else if (location.pathname === '/library') {
-            setActiveMenu('library');
-        } else {
-            setActiveMenu(null);
-        }
-    }, [location]);
+    const { goHome, goBack, goLibrary, activeMenu } = useSidebar();
+    const session = useUserStore((state) => state.user);
 
     return (
         <nav className="hidden sticky top-10 flex-shrink-0 md:block md:w-auto lg:w-[250px]">
@@ -43,7 +19,7 @@ export default function SideBar() {
                     onClick={goLibrary}
                     active={activeMenu === 'library'}
                 />
-                {!user ? (
+                {!session ? (
                     <NavItem icon="/assets/login.svg" label="로그인" onClick={onLogin} />
                 ) : (
                     <NavItem icon="/assets/logout.svg" label="로그아웃" onClick={onLogout} />
