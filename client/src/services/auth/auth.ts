f9c2e3dfa4e2be2b95f0a874a83896b0cc185b17
simuseokyun <sim31059999@gmail.com>
2025-09-02 16:53:14 +0900

    Test

--- client/src/services/auth/auth.ts
+++ client/src/services/auth/auth.ts
@@ -12,7 +12,7 @@ const getWebToken = async () => {
         return { access_token, expires_in };
     }
     try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/webToken`);
+        const response = await axios.get(`/ss/api/webToken`);
         const { access_token, expires_in } = response.data;
         const issuedAt = Date.now();
         localStorage.setItem('webAccessToken', access_token);
