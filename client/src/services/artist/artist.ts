80193109251b41b9ab2226d931dc42cf3334d326
simuseokyun <sim31059999@gmail.com>
2025-09-02 16:16:34 +0900

    Refactor : 배포용 주소 등록

--- client/src/services/artist/artist.ts
+++ client/src/services/artist/artist.ts
@@ -1,19 +1,19 @@
 import { getDataWithAuth } from '../api/client';
 
 const getFollowingArtists = () => {
-    return getDataWithAuth(`/api/me/artists`);
+    return getDataWithAuth(`${import.meta.env.VITE_API_URL}/api/me/artists`);
 };
 const checkArtistFollow = (artistId: string) => {
-    return getDataWithAuth(`/api/me/artists/check?id=${artistId}`);
+    return getDataWithAuth(`${import.meta.env.VITE_API_URL}/api/me/artists/check?id=${artistId}`);
 };
 const artistFollow = (artistId: string) => {
-    return getDataWithAuth(`/api/me/artists/follow`, {
+    return getDataWithAuth(`${import.meta.env.VITE_API_URL}/api/me/artists/follow`, {
         method: 'PUT',
         data: { ids: artistId },
     });
 };
 const artistUnfollow = (artistId: string) => {
-    return getDataWithAuth(`/api/me/artists/unfollow`, {
+    return getDataWithAuth(`${import.meta.env.VITE_API_URL}/api/me/artists/unfollow`, {
         method: 'DELETE',
         data: { ids: artistId },
     });
