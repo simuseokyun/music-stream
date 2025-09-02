a8a1aa3b08d5f85278e53cae9d2aeb45848ae825
simuseokyun <sim31059999@gmail.com>
2025-09-02 23:29:39 +0900

    Test

--- client/src/utils/auth/spotify.ts
+++ client/src/utils/auth/spotify.ts
@@ -1,6 +1,6 @@
 const redirectToSpotify = () => {
-    const client_id = import.meta.env.VITE_CLIENT_ID || '';
-    const redirect_uri = import.meta.env.VITE_REDIRECT_URI || '';
+    const client_id = '9b1aa9c5e93a407f8a2253ef5cd7b2c4';
+    const redirect_uri = 'http://ec2-3-34-190-12.ap-northeast-2.compute.amazonaws.com:8000';
     console.log(client_id);
     if (!client_id || !redirect_uri) {
         throw new Error('인증 정보가 설정되지 않았습니다');
