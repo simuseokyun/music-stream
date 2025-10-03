b9c860113c9059c53ee25402c6bf9011931446ef
simuseokyun <sim31059999@gmail.com>
2025-10-03 17:36:00 +0900

    Test : 음악 재생 성능 확인

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,6 +1,6 @@
 export const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID || '';
 export const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET || '';
 export const REDIRECT_URI = process.env.REDIRECT_URI || '';
-export const BASE_URL_AUTH = 'https://accounts.spotify.com';
-export const BASE_URL_API = 'https://api.spotify.com';
+export const BASE_URL_AUTH = process.env.BASE_URL_AUTH || '';
+export const BASE_URL_API = process.env.BASE_URL_API || '';
 export const BASIC_AUTH = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64');
