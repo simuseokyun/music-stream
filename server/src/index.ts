b6a05a81e78352c393bd14178439603a44760f90
simuseokyun <sim31059999@gmail.com>
2025-10-04 16:35:46 +0900

    Test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
 import dotenv from 'dotenv';
 import path from 'path';
+// dotenv.config({
+//     path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV || 'development'}`),
+// });
+// 1️⃣ 먼저 기본 .env 로드 (NODE_ENV 값이 있든 없든 실행)
+const env = process.env.NODE_ENV || 'development';
+
+// 2️⃣ 해당 환경 파일 로드
 dotenv.config({
-    path: path.resolve(__dirname, `../.env.${process.env.NODE_ENV || 'development'}`),
+    path: path.resolve(__dirname, `../.env.${env}`),
 });
+
 console.log(process.env.NODE_ENV);
 import express from 'express';
 import cors from 'cors';
