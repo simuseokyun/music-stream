52ab25cd498be9da0772a0ac177d1e2728e873e7
simuseokyun <sim31059999@gmail.com>
2025-10-05 18:13:09 +0900

    Refactor : 설정 변경

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'index',
-            script: 'dist/index.js',
+            script: './dist/index.js',
             env: {
                 NODE_ENV: 'development',
             },
